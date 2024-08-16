a795c9a46ddb7709c3e23b3f4a6ba3f028debd28
durshettibalaji <durshettibalaji1@gmail.com>
2024-08-16 13:39:57 +0530

    variables

---
+++ src/General/Variables.java
@@ -0,0 +1,10 @@
+package General;
+public class Variables {
+    public static void main(String[] args) {
+        int age = 231;  // (type of variable) (variable name) = (value of variable)
+        int readableInt = 234_576; // we can use undersqore to increase the readability 
+        System.out.println(age);
+        System.out.println(readableInt);
+    }
+    
+}
