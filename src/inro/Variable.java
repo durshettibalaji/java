e971d9f92bac5adb14d7af8e0b155233971037b8
durshettibalaji <durshettibalaji1@gmail.com>
2025-01-14 12:19:57 +0530

    day1 variable introduction

---
+++ src/inro/Variable.java
@@ -0,0 +1,18 @@
+package src.inro;
+
+public class Variable {
+    public static void main(String[] args) {
+        // integer  => int,long,short,byte
+        int a= 52;
+        byte b= 5;
+        short c = 23;
+        long d =1025l;
+        System.out.println("int :"+a);
+        System.out.println("short :"+c);
+        System.out.println("long :"+d);
+        System.out.println("byte :"+b);
+        char e = 5;
+        System.out.println(e);
+
+    }
+}
